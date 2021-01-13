ee2114adf089943b9fbba05d21910ff24dbfe603
Vanessa Y <vyeung.wt@gmail.com>
2021-01-13 21:04:50 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -21,3 +21,16 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//Logical operators && || !)
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+//! //
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
