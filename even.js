4a226711f34d4091fced34ea02df4eab75fcacca
Vanessa Y <vyeung.wt@gmail.com>
2021-01-14 04:44:38 +0000

    returning function exercise

---
+++ even.js
@@ -0,0 +1,11 @@
+//returns whether function is even// 
+const isEven = function(x){
+  return x % 2 === 0;
+}
+
+const tenIsEven = isEven(10); //this step not necessary
+const elevenIsEven = isEven(11); //this step not necessary
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
